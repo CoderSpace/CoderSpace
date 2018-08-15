e42223a138736e499dd3a22fd01d20d9344dc168
Briana Washington <bwashington1907@gmail.com>
2018-08-15 15:34:03 -0500

    Revert "navbar stuff"

--- js/nav.js
+++ js/nav.js
@@ -1,20 +1,28 @@
 let icon = document.getElementById("icon");
 let nav = document.getElementById("myTopNav");
-icon.addEventListener("click", () => {
-  if (!nav.classList.contains("responsive")) {
-    nav.classList.add("responsive");
-  }
-  else {
-    nav.classList.remove("responsive");
-  }
+let nav2 = document.getElementById("myTopNav2");
+icon.addEventListener("click",() =>{
+  if (!nav.classList.contains("responsive")){
+  nav.classList.add("responsive")
+} else{
+  nav.classList.remove("responsive")
+}
 
 });
-let nav2 = document.getElementById("myTopNav2");
+
+icon.addEventListener("click",() =>{
+  if (!nav2.classList.contains("responsive")){
+  nav2.classList.add("responsive")
+} else{
+  nav2.classList.remove("responsive")
+}
+
+});
+
 window.addEventListener("scroll" ,() =>{
   if (window.pageYOffset > 165) {
     nav2.classList.add("stickynav");
-  }
-  else {
+  } else {
     nav2.classList.remove("stickynav");
   }
 });
